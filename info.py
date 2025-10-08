0b7d4f1316cf58dd9b8b901720660d16d5dba6a6
Sender749 <jotmahal2003@gmail.com>
2025-10-08 11:12:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,7 +29,7 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/Navex_Movies')
 # Pics 
 
 QR_CODE = environ.get('QR_CODE', 'https://envs.sh/iKI.jpg')
-START_IMG = (environ.get('START_IMG', 'https://graph.org/file/7478ff3eac37f4329c3d8.jpg')).split() 
+START_IMG = (environ.get('START_IMG', 'https://i.ibb.co/LdT5fdJY/photo-2025-08-13-01-12-38-7537871916074270724.jpg')).split() 
 FSUB_PICS = (environ.get('FSUB_PICS', 'https://graph.org/file/7478ff3eac37f4329c3d8.jpg')).split() 
 
 # File Limit
